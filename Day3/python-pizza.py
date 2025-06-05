b1ede9cdd9dd38bc883911a2d0a02d523232a780
Marquez Ramirez <marqrami6174@yahoo.com>
2025-06-04 19:58:16 -0700

    Changed the pizza code to be more dry

--- Day3/python-pizza.py
+++ Day3/python-pizza.py
@@ -8,34 +8,26 @@ pepperoni = input("Do you want pepperoni on your pizza? Y or N\n")
 extra_cheese = input("Do you want extra cheese? Y or N\n")
 bill = 0
 
-#creating responses for a small pizza and asking if they want topping finally calculating bill
+
 if size == "S":
   bill += 15
-  if pepperoni == "Y":
-    bill += 2
-  if extra_cheese == "Y":
-    bill += 1
-  print(f"Your final bill is: ${bill}")
-
-#creating responses for a medium pizza and asking if they want topping finally calculating bill
 elif size == "M":
   bill += 20
-  if pepperoni == "Y":
-    bill += 2
-  if extra_cheese == "Y":
-    bill += 1
-  print(f"Your final bill is: ${bill}")
-  
-#creating responses for a large pizza and asking if they want topping finally calculating bill
 elif size == "L":
   bill += 25
-  if pepperoni == "Y":
-    bill += 2
-  if extra_cheese == "Y":
-    bill += 1
-  print(f"Your final bill is: ${bill}")
-  
-#responding to invalid responses
 else:
   print("Please enter a valid size choice: S, M, L")
   
+
+if pepperoni == "Y" and size == "S":
+  bill += 2
+elif pepperoni == "Y" and size == "M" or "L":
+  bill += 3
+else:
+  print("Please select Y or N")
+  
+
+if extra_cheese == "Y":
+  bill += 1
+  
+print(f"Your final bill is: ${bill}.")
