1d0ea6a55e3cb7840b2e7a7a50571682bec3b4bd
Marq6174 <marqrami6174@yahoo.com>
2025-06-06 11:09:55 -0700

    finished treasure island project made it more DRY complient.

--- Day3/treasure-island.py
+++ Day3/treasure-island.py
@@ -1,35 +1,35 @@
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 print("You're at a cross road. Where do you want to go?")
-first_direction = input('Type "left" or "right"\n')
+first_direction = input('Type "left" or "right"\n').lower()
 
-if first_direction == "left" or first_direction == "Left":
+if first_direction == "left":
     print("You've come to a lake. There is an island in the middle of the lake.")
-    wait_or_swim = input('Type "wait" to wait for a boat. Type "swim" to swim across.\n')
+    wait_or_swim = input('Type "wait" to wait for a boat. Type "swim" to swim across.\n').lower()
     
-    if wait_or_swim == "wait" or wait_or_swim == "Wait":
+    if wait_or_swim == "wait":
         print("You've discovered three different doors! which once will you choose?")
-        door_choice = input('Please type "Red", "yellow", or "blue"\n')
+        door_choice = input('Please type "Red", "yellow", or "blue"\n').lower()
         
-        if door_choice == "yellow" or door_choice == "Yellow":
+        if door_choice == "yellow":
             print("Great job! you've found the hidden treasure")
             print("You win!")
         
-        elif door_choice == "red" or door_choice == "Red":
+        elif door_choice == "red":
             print("You've been burned alive by fire!")
             print("Game Over!")
             
-        elif door_choice == "blue" or door_choice == "Blue":
+        elif door_choice == "blue":
             print("You've been devoured by beasts!")
             print("Game Over!")
             
         else:
             print("Game Over!")
         
-    elif wait_or_swim == "swim" or wait_or_swim != "wait" or "Wait":
+    else:
         print("You've been attacked and killed by a trout!")
         print("Game Over!")
         
-elif first_direction == "right" or first_direction != "left" or "Right":
+else:
     print("Oh no! you've fallen into a hole.")
     print("Game Over!")
